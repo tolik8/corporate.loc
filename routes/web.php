ab4dd8486e749dfb24e4bd81cca6b24362c0e46d
tolik8 <tolik8@gmail.com>
2019-06-06 18:03:02 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', 'IndexController', [
+    'only' => ['index'],
+    'names' => ['index' => 'home'],
+]);
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
