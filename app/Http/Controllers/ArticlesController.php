24aa53918a7c94173e070f188469529f4f45a6c7
tolik8 <tolik8@gmail.com>
2019-06-24 17:51:17 +0300

    fix

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -6,16 +6,18 @@ use Illuminate\Http\Request;
 
 use Corp\Repositories\PortfoliosRepository;
 use Corp\Repositories\ArticlesRepository;
+use Corp\Repositories\CommentsRepository;
 use Arr;
 
 class ArticlesController extends SiteController
 {
-    public function __construct(PortfoliosRepository $p_rep, ArticlesRepository $a_rep)
+    public function __construct(PortfoliosRepository $p_rep, ArticlesRepository $a_rep, CommentsRepository $c_rep)
     {
         parent::__construct(new \Corp\Repositories\MenusRepository(new \Corp\Menu));
 
         $this->p_rep = $p_rep;
         $this->a_rep = $a_rep;
+        $this->c_rep = $c_rep;
 
         $this->bar = 'right';
         $this->template = env('THEME') . '.articles';
@@ -28,7 +30,23 @@ class ArticlesController extends SiteController
         $content = view(env('THEME').'.articles_content')->with('articles', $articles)->render();
         $this->vars = Arr::add($this->vars, 'content', $content);
 
+        $comments = $this->getComments(config('settings.recent_comments'));
+        $portfolios = $this->getPortfolios(config('settings.recent_portfolios'));
+        $this->contentRightBar = view(env('THEME').'.articlesBar')->with(['comments' => $comments, 'portfolios' => $portfolios]);
+
         return $this->renderOutput();
+    }
+
+    public function getComments($take)
+    {
+        $comments = $this->c_rep->get(['text', 'name', 'email', 'site', 'article_id', 'user_id'], $take);
+        return $comments;
+    }
+
+    public function getPortfolios($take)
+    {
+        $portfolios = $this->p_rep->get(['title', 'text', 'alias', 'customer', 'img', 'filter_alias'], $take);
+        return $portfolios;
     }
 
     public function getArticles($alias = false)
---
+++ app/Repositories/CommentsRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Corp\Repositories;
+
+use Corp\Comment;
+
+class CommentsRepository extends Repository
+{
+    public function __construct(Comment $comments)
+    {
+        $this->model = $comments;
+    }
+}
