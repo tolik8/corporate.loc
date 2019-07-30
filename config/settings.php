cf163860fee9cc467cc76ee5cba4cbcbefc1988b
tolik8 <tolik8@gmail.com>
2019-07-31 01:26:33 +0300

    fix

--- config/settings.php
+++ config/settings.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'slider_path' => 'slider-cycle',
-    'home_port_count' => 5,
-    'home_articles_count' => 3,
-    'paginate' => 2,
-    'recent_comments' => 3,
-    'recent_portfolios' => 3,
-    'other_portfolios' => 8,
+'slider_path'         => 'slider-cycle',
+'home_port_count'     => 5,
+'home_articles_count' => 3,
+'paginate'            => 2,
+'recent_comments'     => 3,
+'recent_portfolios'   => 3,
+'other_portfolios'    => 8,
 
 ];
